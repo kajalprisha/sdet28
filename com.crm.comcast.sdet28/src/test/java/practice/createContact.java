f7c98e4ebefbc78e49e3d10caa1bd1005835c3ec
Admin <Admin@LAPTOP-3ML7PH3M>
2022-01-20 18:32:10 +0530

    try push

---
+++ com.crm.comcast.sdet28/src/test/java/practice/createContact.java
@@ -0,0 +1,13 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class createContact {
+
+	@Test
+	public void contact() {
+		System.out.println("contact is created");
+	
+	}
+
+}
---
+++ com.crm.comcast.sdet28/src/test/java/practice/createPurchaseOrder.java
@@ -0,0 +1,11 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class createPurchaseOrder {
+	@Test
+	public void order() {
+		System.out.println("created order");
+	}
+
+}
