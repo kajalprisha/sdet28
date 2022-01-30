4b0a87d185c1dd35e496bcf4a99216f6036537ca
kajalprisha <kajalpagare12@gmail.com>
2022-01-30 08:53:57 +0530

    1st push in sdet28

---
+++ com.crm.comcast.sdet28/src/test/java/practice/githubPractice.java
@@ -0,0 +1,12 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class githubPractice {
+
+	@Test()
+	public void githubPractice(){
+		System.out.println("practice makes man perfect");
+	}
+
+}
